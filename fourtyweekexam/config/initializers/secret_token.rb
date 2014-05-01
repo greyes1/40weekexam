1446743676bfa16a019a36f32781c6e581e522dc
Gabriel <greyes@chicagotechacaedmy.org>
2014-05-01 12:57:24 -0500

    init commit

---
+++ fourtyweekexam/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fourtyweekexam::Application.config.secret_key_base = '6423e7e5b9ca5db33e0fa1bcb5509416080dd086d890e2dc6df7da64704bf8419f0d1366e4a1ce37834623f5301247ad13e76e2cc1b6bd858251609702aae8fc'
---
+++ fourtyweekexam/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fourtyweekexam::Application.config.session_store :cookie_store, key: '_fourtyweekexam_session'
